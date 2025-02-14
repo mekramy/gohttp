3b419adc9d733ff5092e38d5b6aeeb3e71ca7334
M Ekramy <m.ajamekramy@gmail.com>
2025-02-14 21:40:47 +0330

    fix middleware naming

--- middleware/formonly.go
+++ middleware/formonly.go
@@ -4,11 +4,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// FormOrURLEncodedOnly is a middleware that ensures the request's Content-Type is either
+// FormOnly is a middleware that ensures the request's Content-Type is either
 // "application/x-www-form-urlencoded" or "multipart/form-data".
 // If the Content-Type is neither of these, it will execute the optional onFail handler
 // if provided, or return a 406 Not Acceptable status by default.
-func FormOrURLEncodedOnly(onFail ...fiber.Handler) fiber.Handler {
+func FormOnly(onFail ...fiber.Handler) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		contentType := c.Get("Content-Type")
 		if contentType != "application/x-www-form-urlencoded" && contentType != "multipart/form-data" {
