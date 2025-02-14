1286b813a557348f47509527a58e89ebdb9552ab
M Ekramy <m.ajamekramy@gmail.com>
2025-02-14 21:17:32 +0330

    add CSRF middleware and helpers

---
+++ session/csrf.go
@@ -0,0 +1,72 @@
+package session
+
+import (
+	"errors"
+	"slices"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// NewCSRF creates a new CSRF middleware handler for Fiber. It validates the CSRF token
+// for incoming requests and generates a new token if needed. The secure argument specifies
+// whether the CSRF cookie should be generated securely. The onFail function is called if the
+// CSRF validation fails, and the next function can be used to skip CSRF validation for certain
+// requests. By default, this middleware generates a 419 HTTP response if CSRF validation fails.
+// This middleware must be called after the session middleware.
+func NewCSRF(secure bool, onFail func(*fiber.Ctx) error, next func(*fiber.Ctx) bool) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// Skip
+		if next != nil && next(c) {
+			return c.Next()
+		}
+
+		// Set allowed headers
+		c.Append("Access-Control-Expose-Headers", "X-CSRF-TOKEN")
+		c.Append("Access-Control-Allow-Headers", "X-CSRF-TOKEN")
+
+		// Parse session
+		session := Parse(c)
+		if session == nil {
+			return errors.New("failed to resolve session")
+		}
+
+		// Get CSRF token
+		token := session.Cast("csrf").StringSafe("")
+
+		// Generate or refresh token if needed
+		if token == "" {
+			token, _ = RefreshCSRF(secure, c, session)
+		}
+
+		// Ignore RFC9110 (GET, HEAD, OPTIONS, and TRACE) methods
+		if slices.Contains([]string{fiber.MethodPost, fiber.MethodPut, fiber.MethodPatch, fiber.MethodDelete}, c.Method()) {
+			// Parse request csrf token from X-CSRF-Token header
+			input := c.Get("X-CSRF-Token")
+
+			// Parse request csrf token from csrf_token cookie
+			if input == "" {
+				input = c.Cookies("csrf_token")
+			}
+
+			// Parse request csrf token from csrf_token form
+			if input == "" {
+				type Form struct {
+					Token string `json:"csrf_token" form:"csrf_token"`
+				}
+				var inp Form
+				c.BodyParser(&inp)
+				input = inp.Token
+			}
+
+			// Validate CSRF
+			if token == "" || input != token {
+				if onFail != nil {
+					return onFail(c)
+				}
+				return c.Status(419).SendString("invalid csrf token")
+			}
+		}
+
+		return c.Next()
+	}
+}
--- session/funcs.go
+++ session/funcs.go
@@ -1,7 +1,10 @@
 package session
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 // Parse extracts the Session object from the fiber.Ctx context.
@@ -14,4 +17,48 @@ func Parse(ctx *fiber.Ctx) Session {
 	}
 
 	return nil
+}
+
+// CSRF retrieves the CSRF token from the session associated with the given Fiber context.
+// Returns an empty string if the session is nil or the "csrf" value is not found.
+func CSRF(ctx *fiber.Ctx) string {
+	session := Parse(ctx)
+	if session != nil {
+		session.Cast("csrf").StringSafe("")
+	}
+	return ""
+}
+
+// RefreshCSRF generates a new CSRF token, sets it in the response headers and cookies,
+// and saves it to the session. It returns the generated token or an error if the session
+// not passed and cannot be resolved.
+func RefreshCSRF(secure bool, c *fiber.Ctx, s Session) (string, error) {
+	token := uuid.NewString()
+
+	// Parse session if not passed
+	if s == nil {
+		s = Parse(c)
+		if s == nil {
+			return "", errors.New("failed to resolve session")
+		}
+	}
+
+	// Set headers
+	c.Append("Access-Control-Expose-Headers", "X-CSRF-TOKEN")
+	c.Append("Access-Control-Allow-Headers", "X-CSRF-TOKEN")
+	c.Set("X-CSRF-TOKEN", token)
+
+	// Set Cookie
+	c.Cookie(&fiber.Cookie{
+		Name:     "csrf_token",
+		Value:    token,
+		HTTPOnly: true,
+		Secure:   secure,
+		SameSite: fiber.CookieSameSiteStrictMode,
+	})
+
+	// Save to session
+	s.Set("csrf", token)
+
+	return token, nil
 }
