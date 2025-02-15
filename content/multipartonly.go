afe5e12da48740ab69bd48a106791849cd2c27ec
M Ekramy <m.ajamekramy@gmail.com>
2025-02-15 21:08:00 +0330

    add multipart form content middleware

---
+++ content/multipartonly.go
@@ -0,0 +1,21 @@
+package content
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// MultipartOnly is a middleware that ensures the request's Content-Type is "multipart/form-data"
+// If the Content-Type is neither of these, it will execute the optional onFail handler if provided,
+// or return a 406 Not Acceptable status by default.
+func MultipartOnly(onFail ...fiber.Handler) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		contentType := c.Get("Content-Type")
+		if contentType != "multipart/form-data" {
+			if len(onFail) > 0 && onFail[0] != nil {
+				return onFail[0](c)
+			}
+			return c.SendStatus(fiber.StatusNotAcceptable)
+		}
+		return c.Next()
+	}
+}
